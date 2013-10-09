ee4219b8de24f2a802b63af0215c1ed56a0896a7
Benoît Garçon <benoit.garconbesson@gmail.com>
2013-10-09 09:21:53 +0200

    Test size 1

--- Dames/ressources/sources/utile.h
+++ Dames/ressources/sources/utile.h
@@ -22,9 +22,9 @@
 #define CY_SCREEN           GetSystemMetrics(SM_CYSCREEN)            ///Hauteur
 #define NB_CARRE_X          10
 #define NB_CARRE_Y          10
-#define TAILLE_CARRE        GetSystemMetrics(SM_CYSCREEN) / 10
-#define LARGEUR_ECRAN       CX_SCREEN                   ///  1366    ///  1920
-#define HAUTEUR_ECRAN       size_of_game(CY_SCREEN)     ///  768     ///  1080
+#define TAILLE_CARRE        HAUTEUR_ECRAN / 10
+#define LARGEUR_ECRAN       CX_SCREEN                   ///     800     ///  1366    ///  1920
+#define HAUTEUR_ECRAN       size_of_game(CY_SCREEN)     ///     600     ///  768     ///  1080
 #define SCREEN_REFRESH      40
 #define DEBUG               std::cerr << std::endl <<
 #define NOMBRE_IMAGE        7
