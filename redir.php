436c6fd39231bebfff4289550ee6a89cbeea1521
Benoît Garçon <benoit.garconbesson@gmail.com>
2013-11-29 15:06:44 +0100

    test php

---
+++ redir.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+
+<title>Redirection</title>
+<meta http-equiv="refresh" content="5; URL=https://github.com/begarco/BG_Prod/zipball/master">
+</head>
+
+<body>
+Redirection vers           <a href="https://github.com/begarco/BG_Prod/tarball/master" class="button">
+            <small>Download</small>
+            .tar.gz file
+          </a> dans 5 secondes.
+		  
+<?php echo "patate" ;?>
+</body>
+
+</html> 
