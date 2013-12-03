56fa692cd3f98d8522abd1e65409d06e02e3926c
Benoît Garçon <benoit.garconbesson@gmail.com>
2013-12-03 12:02:05 +0100

    Surprise

--- Dames/ressources/sources/utile.h
+++ Dames/ressources/sources/utile.h
@@ -1,6 +1,6 @@
 /*
     Draughts - Les Dames
-    Copyright (C) 2013  Garçon Benoît
+    Copyright (C) 2013  BG Prod
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    Contact me : benoit.garconbesson@gmail.com
+    Contact me : bgprod@outlook.com
 */
 
 #ifndef UTILE_H_INCLUDED
